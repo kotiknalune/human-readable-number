2b76a5d3c8acbd1f99f50ef7c8a34ac09e7f8a51
Katerina <katerina.raylyan@gmail.com>
2020-02-24 21:43:49 +0300

    implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
-  
+
+let smallNumbers = ["zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
+let tensNumbers = ["","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
+let numberText = "";
+
+if(number == 0) return smallNumbers[0];  
+
+let hundreds = Math.floor(number / 100), // how many whole hundreds in number
+	 tensUnits = number % 100; // remainder
+
+if(hundreds >= 1) {
+	 		numberText += smallNumbers[hundreds] + " hundred ";
+	 }
+
+let tens = Math.floor(tensUnits / 10), //how many whole tens in number
+	 units = tensUnits % 10; // how many units in number
+
+if(tens >= 2) {
+	 	numberText += tensNumbers[tens];
+	 	if(units != 0){
+	 		numberText += " " + smallNumbers[units];
+	 	}
+	 }
+else if (tensUnits != 0) {
+		numberText += smallNumbers[tensUnits];
+    }	
+return numberText.trim();
 }
